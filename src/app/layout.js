248c6f210f40cd292948fe9ed85d049e11ced91d
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-02 20:17:14 +0100

    fix: text copy, links, and resume file, add some layout tweaks

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "Jasmin Gubeljic",
+  title: "Jasmin Gubeljić",
   description: "Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -34,18 +34,18 @@ export default function Home() {
       <main>
         <p className={styles["about-me"]}>
           I am nearing the completion of my portfolio, and in the meantime, I
-          invite you to explore some of my repositories. For a remarkable
-          front-end showcase, I suggest taking a look at a{" "}
+          invite you to explore some of my repositories. For a glimpse into my
+          front-end capabilities, I suggest taking a look at a{" "}
           <a
             href="https://github.com/jasmingubeljic/estore-client"
             target="_blank"
             className={styles["repository-link"]}
           >
-            shop project
+            shop
           </a>{" "}
-          built with React. Additionally, my{" "}
+          project built with React,{" "}
           <a
-            href="https://github.com/jasmingubeljic/estore-client"
+            href="https://github.com/jasmingubeljic/movimentum"
             target="_blank"
             className={styles["repository-link"]}
           >
@@ -53,27 +53,34 @@ export default function Home() {
           </a>{" "}
           and{" "}
           <a
-            href="https://github.com/jasmingubeljic/estore-client"
+            href="https://github.com/jasmingubeljic/stoille"
             target="_blank"
             className={styles["repository-link"]}
           >
             Stoille
           </a>{" "}
-          projects highlight my proficiency in Vanilla CSS and Vanilla JS. If
-          you have an interest in back-end development, you will also find a{" "}
+          projects highlight my proficiency in Vanilla CSS. If you have an
+          interest in back-end development, you will also find a{" "}
           <a
             href="https://github.com/jasmingubeljic/estore-server"
             target="_blank"
             className={styles["repository-link"]}
           >
-            Node JS
+            eStore
           </a>{" "}
-          project available for your review.
+          (Node JS) available for your review.
         </p>
       </main>
       <footer>
         <ul>
-          <li>jasmin.g@tutanota.com</li>
+          <li>
+            <a href="mailto: jasmin.g@tutanota.com">jasmin.g@tutanota.com</a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/jasmin-gubeljic" target="_blank">
+              LinkedIn
+            </a>
+          </li>
         </ul>
       </footer>
     </div>
