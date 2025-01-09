4715617b4c4736666d98b7f16e0c6a3d58ad52be
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-09 19:47:18 +0100

    feat: add not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+export default function NotFound({ error }) {
+  return (
+    <main>
+      <h1>404 - Page Not Found</h1>
+      <p>
+        Oops! It seems that the page you're looking for doesn't exist. It might
+        have been moved, deleted, or you may have typed the wrong URL.
+      </p>
+    </main>
+  );
+}
