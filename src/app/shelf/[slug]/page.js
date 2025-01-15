594c9ec7746139229bb74bfec075d8ec2549eea7
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-15 17:24:25 +0100

    fix: generateStaticParams

--- src/app/shelf/[slug]/page.js
+++ src/app/shelf/[slug]/page.js
@@ -3,3 +3,11 @@ import Article1 from "../../../components/Articles/Article1";
 export default function Article() {
   return <Article1 />;
 }
+
+export async function generateStaticParams() {
+  const slugs = ["debounce-function-issue-in-useEffect"]; // static slugs
+
+  return slugs.map((slug) => ({
+    slug: slug,
+  }));
+}
