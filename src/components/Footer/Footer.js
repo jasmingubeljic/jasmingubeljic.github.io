fc82f6f6fa3e61a4567c84c1ff4b194fc87bf6d3
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-17 22:13:46 +0100

    style: use icons for contact info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,23 @@
 import styles from "./Footer.module.css";
+import { MdMail } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <ul className={styles.ul}>
         <li className={styles.li}>
-          <a href="mailto: jasmin.g@tutanota.com" className={styles.link}>
-            jasmin.g@tutanota.com
-          </a>
-        </li>
-        <li className={styles.li}>
           <a
             href="https://linkedin.com/in/jasmin-gubeljic"
             target="_blank"
             className={styles.link}
           >
-            LinkedIn
+            <FaLinkedinIn />
+          </a>
+        </li>
+        <li className={styles.li}>
+          <a href="mailto: jasmin.g@tutanota.com" className={styles.link}>
+            <MdMail />
           </a>
         </li>
       </ul>
